8191393d40e3dd28009b701cf47605977b45a6d5
Jake Harris <milieu@javakat.io>
2022-03-23 10:16:00 -0500

    02 - Should('be.visible')

--- projects/earnings-app-e2e/src/integration/login-page.spec.ts
+++ projects/earnings-app-e2e/src/integration/login-page.spec.ts
@@ -4,4 +4,10 @@ describe('Login Page', () => {
 
     cy.get('h3').contains('Login');
   });
+
+  it('should display the login form', () => {
+    cy.visit('/');
+
+    cy.get('form').should('be.visible');
+  });
 });
