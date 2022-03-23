d58291cf52be3e07ca9205f64b86d44354dee273
Jake Harris <milieu@javakat.io>
2022-03-23 14:39:46 -0500

    11 - SIFERS

--- projects/earnings-app-e2e/src/integration/books-page.spec.ts
+++ projects/earnings-app-e2e/src/integration/books-page.spec.ts
@@ -7,39 +7,16 @@ import * as BookFormComponent from '../support/book-form-component.harness';
 import * as BooksPage from '../support/books-page.harness';
 
 describe('Books Page', () => {
-  let book: BookModel;
-
-  beforeEach(() => {
-    book = {
-      id: uuid.v4(),
-      name: 'The Lord of the Rings',
-      earnings: '100',
-      description:
-        'The Lord of the Rings is an epic high fantasy novel written by English author and scholar J. R. R. Tolkien.',
-    };
-
-    BooksApi.deleteAllBooks();
-    BooksApi.createBook(book);
-
-    AuthApi.login('Admin', 'password');
-
-    cy.intercept('GET', 'http://localhost:3000/books').as('getBooks');
-    cy.visit('/');
-    cy.wait('@getBooks');
-  });
-
   it('should show a list all of the books', () => {
+    const book = setup();
+
     BookListComponent.getBook(book.id).contains(book.name);
   });
 
   it('should gracefully show an error message when loading the books fails', () => {
-    cy.intercept('GET', 'http://localhost:3000/books', {
-      statusCode: 500,
-    }).as('getBooksError');
-    cy.visit('/books');
-    cy.wait('@getBooksError');
+    setup({ throwErrorWhenLoadingBooks: true });
 
-    BooksPage.getError();
+    BooksPage.getError().should('contain', 'Error');
   });
 
   // it('should let you create a book', () => {});
@@ -48,3 +25,31 @@ describe('Books Page', () => {
 
   // it('should let you delete a book', () => {});
 });
+
+function setup(options?: { throwErrorWhenLoadingBooks?: boolean }) {
+  const book = {
+    id: uuid.v4(),
+    name: 'The Lord of the Rings',
+    earnings: '100',
+    description:
+      'The Lord of the Rings is an epic high fantasy novel written by English author and scholar J. R. R. Tolkien.',
+  };
+
+  BooksApi.deleteAllBooks();
+  BooksApi.createBook(book);
+
+  AuthApi.login('Admin', 'password');
+
+  if (options?.throwErrorWhenLoadingBooks) {
+    cy.intercept('GET', 'http://localhost:3000/books', {
+      statusCode: 500,
+    }).as('getBooks');
+  } else {
+    cy.intercept('GET', 'http://localhost:3000/books').as('getBooks');
+  }
+
+  cy.visit('/');
+  cy.wait('@getBooks');
+
+  return book;
+}
